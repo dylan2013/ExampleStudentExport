a1dbd22cb61e1b8c68facdeacee11a681e1661bc
dylan2013 <dylan.huang@ischool.com.tw>
2013-07-16 12:41:43 +0800

    匯出學生基本資料功能

---
+++ ExampleStudentExport/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FISCA;
+using FISCA.Presentation;
+
+namespace ExampleStudentExport
+{
+    public class Program
+    {
+        [MainMethod()]
+        static public void Main()
+        {
+
+            RibbonBarButton rbItemExport = K12.Presentation.NLDPanels.Student.RibbonBarItems["資料統計"]["匯出"];
+            rbItemExport["學籍相關匯出"]["匯出學生基本資料_改"].Click += delegate
+            {
+                ExportWizard export = new ExportWizard();
+                export.ShowDialog();
+            };
+        }
+    }
+}
